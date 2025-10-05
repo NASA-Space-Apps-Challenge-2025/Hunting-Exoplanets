de8ab4b7699338bf83a286c15ddfa1231b7792c3
Hassan nauman <hassannauman_1@hotmail.com>
2025-10-05 17:00:54 -0500

    Final commit

--- exoplanetPrediction/next.config.ts
+++ exoplanetPrediction/next.config.ts
@@ -3,5 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const isProd = process.env.NODE_ENV === 'production';
 
+module.exports = {
+  output: 'export',                 // static export
+  images: { unoptimized: true },    // no Image Optimization on GH Pages
+  basePath: '/Hunting-Exoplanets',  // repo name
+  assetPrefix: '/Hunting-Exoplanets/',
+  trailingSlash: true               // helps with GH Pages routing
+};
 export default nextConfig;
